2ecbc6727cae1fd9859b16e8f6551ec8ef4ea361
Ezarreinhartbanne <ezarganteng222123@gmail.com>
2024-09-27 15:17:42 +0800

    Add files via upload

---
+++ Apd pratikum/2409106086_ezar reinhart banne_B2_24.py
@@ -0,0 +1,47 @@
+# ulang = 10
+# for i in range(ulang):
+     # print("perulangan ke-"+str(i)+" kali")
+     # print (f"perulangan ke--{i}kali")
+
+# for i in range(1, 11, 3):
+    # print (f"perulangan ke---{i} kali")
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+ #         print(f"{i} x {j} = {i * j}")
+ #    print()
+ 
+# jawab = 'ya'
+# hitung = 0
+# while(jawab == 'ya'):
+#    hitung +=1
+#    jawab = input("ulang lagi tidak? ")
+# print(f"Total perulangan: {hitung}")
+
+# hitung = 0
+# while True:
+#    hitung += 1
+#    ulang = input("masih ingin mengulang? ")
+#    if ulang == "tidak" or ulang =="Tidak":
+#        break
+# print(f"Total perulangan: {hitung}")
+
+# print("Daftar bilangan ganjil dari 1-10")
+# for i in range (10):
+#    if i % 2 == 0:
+#        continue
+#    print(i)
+
+#usn = "admin"
+#pw = "admin123"
+#salah = 0
+
+# while salah < 3:
+ #      
+ #     password = input("Masukan password: ")
+ #   if usn == username and pw == password:
+ #       print("login berhasil")
+ #       break
+ #   else: 
+ #       print("login gagal")
+  #      salah += 1
