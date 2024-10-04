11fcdefe0999cc98ebf9fcfc7bf2014a0affd516
Ezarreinhartbanne <ezarganteng222123@gmail.com>
2024-10-04 15:28:44 +0800

    Add files via upload

---
+++ Apd pratikum/python ezar/python.py
@@ -0,0 +1,61 @@
+#tas = ["buku", 32, True, 3.14, ["IF", 24]]
+
+#print(tas)
+
+#tas = ["buku", 32, True, 3.14, ["IF",24]]
+  
+#print(tas[0])
+
+#matkul = ["kalkulus", "fisdas", "pti"]
+#print (matkul)
+
+#matkul.append("logika informatika")
+#print(matkul)
+
+#prodi = ["informatika", "sisfor", "arsitektur", "tekling"]
+#print(prodi)
+
+#prodi[2] = "teknik lingkungan"
+#print(prodi)
+
+#prodi = ["informatika", "sistem informasi", "arsitektur", "teknik lingkungan"]
+#print(prodi)
+
+#del prodi[2]
+#print(prodi)
+
+#prodi = ["informatika", "sistem informasi", "teknik arsitektur", "teknik lingkungan", "teknik pertambangan", "teknik elektro", "teknik industri", "teknik sipil", "teknik geologi", "teknik kimia"]
+
+#prodi(prodi[0:6])
+
+#barang = [['sepatu", "tas", "baju"], 
+        #   ["pulpen", "pensil", "laptop"]]
+#print(barang [1][1])
+
+#barang = [["sepatu", "kaos kaki", "sarung"], ["pulpen", "pensil", "laptop"]]
+
+#for i in barang:
+ #   for j in i:
+ #       print(j)
+
+#nama = ("aldi" "daffa", "arisyhi")
+
+#print(nama)
+#nama.append("rava")
+#print(nama)
+
+#anggota = ("aldy" "rizky", "abdullah", "milos", "reza",)
+
+#print(f"anggota AI yang ketiga adalah {anggota[3]}")
+
+#for i in anggota :
+   #  print(i)
+     
+#print("banyak anggota AI sebanyak : %d" %len(anggota))
+
+mahasiswa = (69, "informatika", "2209106044", "aldy septian ")
+print(mahasiswa)
+mahasiswa = list(mahasiswa)
+mahasiswa[2] = "2409106086"
+
+mahasiswa = tuple
