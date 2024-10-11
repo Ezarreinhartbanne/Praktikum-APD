5efd76185264f1c011b887b709cce7348eaa041c
Ezarreinhartbanne <ezarganteng222123@gmail.com>
2024-10-11 15:05:20 +0800

    Add files via upload

---
+++ Apd pratikum/pertemuan6.py
@@ -0,0 +1,36 @@
+#daftar_buku = {}
+
+#daftar_buku["novel 1"]
+
+#nama_dict = {
+ #   "key": "value"
+
+#}
+
+#daftar_buku["novel 1"] 
+
+#daftar_buku = dict(buku1 = "harry potter", buku2 = "percy jackson")
+#print (daftar_buku)
+ 
+#nilai = {
+#    "matematika" : 80,
+ #   "b. indonesia" : 90,
+ #   "b. inggris" : 81,
+ #   "kimia" : 78,
+ #   "fisika" : 80
+ #   }
+
+#for i in nilai:
+ #    print(i)
+
+# print(f"jumlah elemen dari variabel dict nilai adalah {len}")
+
+#daftar_nilai = nilai.copy()
+#print(daftar_nilai)
+
+key = "motor", "mobil", "sepeda"
+value = 2
+daftar_kendaraan = dict.fromkeys(key, value)
+
+print(daftar_kendaraan)
+
