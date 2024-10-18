40e44343c7695f0a302bf309bece49b3c4bfef2a
Ezarreinhartbanne <ezarganteng222123@gmail.com>
2024-10-18 15:06:39 +0800

    Add files via upload

---
+++ Apd pratikum/pertemuan7.py
@@ -0,0 +1,28 @@
+#def cetak (nama, nim, **matkul):
+#    print(nama, nim,matkul)
+
+#cetak("fathir",41, "kalkulus", "fisika dasar")
+
+#var = 2, 4,5 
+
+#nama = "informatika"
+#mata_kuliah = "algoritma dan permograman dasar"
+#kelas = "it asik"
+
+#def info():
+ #   nama = "teknik elektro"
+ #   mata_kuliah = "pengantar teknik elektro"
+ #   print("prodi:", nama)
+ #   print("mata kuliah:", mata_kuliah)
+   # print(kelas)
+#info()
+#print("prodi:", nama)
+#print("mata kuliah:", mata_kuliah)
+
+
+
+
+
+
+
+
