af1ec618d12197b3e1c9670742585320e0f7cf4f
Ezarreinhartbanne <ezarganteng222123@gmail.com>
2024-10-25 15:29:27 +0800

    Add files via upload

---
+++ Apd pratikum/pertmuan8.py
@@ -0,0 +1,53 @@
+#if cx < 10
+ #   print ("x kurang dari 10")
+#else:
+ #   print("x lebih dari 10")
+
+#harga = [1000, 2000, 3000]
+#print(harga[8])
+
+#umur = 15
+#umur.uper()
+
+#import skibidi
+
+#buku = {
+ #   "buku1" : "matematika",
+ #   "buku2" : "fisika",
+#}
+#print(buku["buku3"])
+#print(buku["buku2020202"])
+
+#try:
+ #   harga_barang = int(input("masukan harga barang:"))
+ #   print(harga_barang)
+#expect value error as e:
+#print(e)
+#print ("inputan harus berupa angka")
+#expect expection:
+#   else:
+ #      print(harga_barang)
+#finally:
+ #    print(("program selesai"))
+
+#def panggil ():
+#    try:
+ #       umur = int(input("masukan umur anda:"))
+ #   expect valueerror:
+#      print("input harus berupa angka")
+ #   panggil()
+#else:
+#    print("umur anda adalah", umur)
+
+#panggil()
+
+
+
+
+
+
+
+
+
+
+
